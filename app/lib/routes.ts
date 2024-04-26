df0d2d6f4c649c7d9ce41eefbd9ce258389dd0f9
NhicolasAponte <Nhic.Aponte@gmail.com>
2024-04-25 23:50:21 -0500

    git upstream test - git

---
+++ app/lib/routes.ts
@@ -0,0 +1,14 @@
+export const routes = [
+  {
+    label: "New Design",
+    href: "/design_dashboard",
+  },
+  {
+    label: "Check Order Status",
+    href: "/order_status",
+  },
+  {
+    label: "Browse Templates",
+    href: "/browse_templates",
+  },
+];
