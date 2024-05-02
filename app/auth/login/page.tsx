fc2366c425550de5a09bd21a76a510a66366f7ad
NhicFlores <nflores@mankowindows.com>
2024-05-02 16:48:14 -0500

    stubbed in auth routes, components, and layout

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from '@/components/auth/LoginForm'
+//keep page as a server component
+// tag components as client components so they can manage state
+
+const LoginPage = () => {
+  return (
+    <LoginForm/>
+  )
+}
+
+export default LoginPage
---
+++ components/auth/CardWrapper.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CardWrapper = () => {
+  return (
+    <div>
+      Card Wrapper
+    </div>
+  )
+}
+
+export default CardWrapper
