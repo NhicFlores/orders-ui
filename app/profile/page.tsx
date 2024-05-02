e3402056366fdd6275123a21fe306b16f6bafa05
NhicFlores <nflores@mankowindows.com>
2024-05-02 09:24:32 -0500

    added form and form validation - need submit function

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,14 @@
 //profile route
 
+import Container from "../ui/components/page-container";
 import ProfileForm from "../ui/components/profile-components/profile-form";
 
 export default function Page() {
     return (
-        <main className="m-5">
-            <ProfileForm/>
+        <main className="mt-5">
+            <Container>
+                <ProfileForm/>
+            </Container>
         </main>
     )
 }
--- app/ui/components/profile-components/profile-form.tsx
+++ app/ui/components/profile-components/profile-form.tsx
@@ -14,24 +14,32 @@ import {
     FormMessage    
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-//NOTE TODO: email validation, phone number validation 
+//NOTE TODO: email validation, phone number validation - regex might work
 //nested object schema with z.record
 const formSchema = z.object({
-    name: z.string().min(4, {
-        message: "Username must be at least 4 characters.",
+    name: z.string({
+        invalid_type_error: 'Please enter your name'
     }),
     email: z.string().email({
         message: "This is not a valid emaill address."
     }),
-    phone_num: z.string().min(10, {
-        message: "Enter a valid phone number."
+    phone_num: z.string({
+        invalid_type_error: 'please enter a valid phone number.'
     }),
     billing_info: z.object({
-        billing_addr: z.string().min(5, {
-            message: "invalid characters in address"
+        billing_addr: z.string({
+            invalid_type_error: 'Please enter a valid street address.'
         }),
-    })
-})
+        payment_method: z.string({
+            invalid_type_error: 'select payment method'
+        }),
+    }),
+    shipping_info: z.object({
+        delivery_addr: z.string({
+            invalid_type_error: 'Please enter a valid street address.'
+        }),
+    }),
+});
 
 export default function ProfileForm(){
 
@@ -39,14 +47,30 @@ export default function ProfileForm(){
         resolver: zodResolver(formSchema),
         defaultValues: {
             name: "",
+            email: "",
+            phone_num: "",
+            billing_info: {
+                billing_addr: "",
+                payment_method: "",
+            },
+            shipping_info: {
+                delivery_addr: "",
+            },
         },
     })
     //NOTE TODO: onSubmit event handler 
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log("user form");
-        console.log(form);
-    }
+    // function onSubmit(values: z.infer<typeof formSchema>) {
+    //     console.log("user form");
+    //     console.log(form);
+    // }
 
+    function clickHandler(){
+        console.log("click handler");
+        console.log(form.formState);
+    }
+    //create multiple forms - one for customer info, billing info, delivery info
+    //pass that form data from each back to the action 
+    //in action, build the object you're going to send back to the db 
     return (
         <Form {...form}>
             <form>
@@ -82,7 +106,59 @@ export default function ProfileForm(){
                         </FormItem>
                     )}
                 />
-                <Button type="submit">Save</Button>
+                <FormField 
+                    control={form.control}
+                    name="phone_num"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Phone Number</FormLabel>
+                            <FormControl>
+                                <Input placeholder="(000)000-0000" {...field}/>
+                            </FormControl>
+                            <FormDescription>
+                                Primary Phone Number
+                            </FormDescription>
+                            <FormMessage/>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="billing_info.billing_addr"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Billing Address</FormLabel>
+                            <FormControl>
+                                <Input placeholder="street address" {...field}/>
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="billing_info.payment_method"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Payment Method</FormLabel>
+                            <FormControl>
+                                <Input placeholder="credit..." {...field}/>
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="shipping_info.delivery_addr"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Delivery Address</FormLabel>
+                            <FormControl>
+                                <Input placeholder="street address" {...field}/>
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit" onClick={clickHandler}>Save</Button>
             </form>
         </Form>
     )
