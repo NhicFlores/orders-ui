01585046d289f82ea87ac05296d847545e1db900
NhicFlores <nflores@mankowindows.com>
2024-05-17 11:55:13 -0500

    dimensions route and card stubbed in - need db update to populate data - need fetch functions

--- components/product-components/DimensionsCard.tsx
+++ components/product-components/DimensionsCard.tsx
@@ -1,10 +1,97 @@
-
+import { Button } from "../ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card"
+import { Label } from "../ui/label"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select"
+import { Square } from "lucide-react";
 
 const DimensionsCard = () => {
+
+    const rangeInInches = ["1", "2","3", "4","5", "6","7", "8",];
+    const fractions = ['1/8', '5/32', '3/16', '1/4', '5/16', '3/8', '1/2', '5/8'];
+    // NOTE TODO: each shape needs to have required dimensions associated with it 
+    //each dimension needs label and options array 
   return (
-    <div>
-      dimensions card
-    </div>
+    <Card className="w-[350px]">
+        <CardHeader>
+            <CardTitle>Dimensions</CardTitle>
+            <CardDescription>
+                Enter dimensions: all units are in inches
+            </CardDescription>
+        </CardHeader>
+        <CardContent className="px-4">
+            <div>
+                <Square width={200} height={200}/>
+            </div>
+            <form className="space-y-6">     
+                <div className="space-y-2">
+                    <Label htmlFor="width">Width (inches)</Label>
+                    <div className="flex space-x-2">
+                        <Select>
+                            <SelectTrigger id="width">
+                                <SelectValue placeholder="1"/>
+                            </SelectTrigger>
+                            <SelectContent position="popper">
+                                {rangeInInches.map((value, index) => (
+                                    <div key={index}>
+                                        <SelectItem value={value}>{value}</SelectItem>
+                                    </div>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                        <Select>
+                            <SelectTrigger id="width">
+                                <SelectValue placeholder="1/8"/>
+                            </SelectTrigger>
+                            <SelectContent>
+                                {fractions.map((value, index) => (
+                                    <div key={index}>
+                                        <SelectItem value={value}>{value}</SelectItem>
+                                    </div>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                    </div>
+                </div> 
+                <div className="space-y-2">
+                    <Label>Height (inches)</Label>
+                    <div className="flex space-x-2">
+                        <Select>
+                            <SelectTrigger id="height">
+                                <SelectValue placeholder="1"/>
+                            </SelectTrigger>
+                            <SelectContent>
+                                {rangeInInches.map((value, index) => (
+                                    <div key={index}>
+                                        <SelectItem value={value}>{value}</SelectItem>
+                                    </div>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                        <Select>
+                            <SelectTrigger id="height">
+                                <SelectValue placeholder="1/8"/>
+                            </SelectTrigger>
+                            <SelectContent>
+                                {fractions.map((value, index) => (
+                                    <div key={index}>
+                                        <SelectItem value={value}>{value}</SelectItem>
+                                    </div>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                    </div>
+                </div>
+                <div className="flex justify-between">
+                    <Button variant='outline'>
+                        Back
+                    </Button>
+                    <Button type="submit">
+                        Continue
+                    </Button>
+                </div>  
+            </form>
+        </CardContent>
+    </Card>
   )
 }
 
