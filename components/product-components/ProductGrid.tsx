2a641fda054c2cd5bcbe41b033a4a7c8c8db2487
NhicFlores <nflores@mankowindows.com>
2024-05-17 12:02:44 -0500

    added back and continue buttons to product grid

--- components/product-components/ProductGrid.tsx
+++ components/product-components/ProductGrid.tsx
@@ -2,6 +2,7 @@
 import glassImage from '@/public/images/glass-verre.jpg';
 import ProductCard from './product-card';
 import { useState } from 'react';
+import { Button } from '../ui/button';
 
 const productList = [
     {
@@ -52,9 +53,17 @@ export default function ProductGrid(){
 
   return (
     <div className='container mx-auto p-4'>
-      <h1 className='text-2xl font-bold mb-4'>
-        Select Glass Type
-      </h1>
+      <div className='flex justify-between'>
+        <Button variant={'outline'}>
+          Back
+        </Button>
+        <h1 className='text-2xl font-bold mb-4'>
+          Select Glass Type
+        </h1>
+        <Button>
+          Continue
+        </Button>
+      </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {productList.map((item, i) => (
             <ProductCard
