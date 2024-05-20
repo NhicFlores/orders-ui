d4d3b961c8debc7de69ffa7a6981cf8d43d31c51
NhicFlores <nflores@mankowindows.com>
2024-05-20 13:35:07 -0500

    minor styling change so header buttons change variant based on which page you are on

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,11 +1,14 @@
+'use client';
 import Container from "./components/page-container";
 import Link from "next/link";
 import { Dashboard, HeaderRoutes, Profile } from "../lib/routes";
 import { Button } from "@/components/ui/button";
 import ProfileButton from "./components/profile-button";
+import { usePathname } from "next/navigation";
 
 //might need to install previous version of lucide npm i lucide-react@0.263.1 if this version is still having issues
 export default function Header() {
+  const pathName = usePathname();
   return (
     <header className="sm:flex sm:justify-between py-3 border-b">
       <Container>
@@ -17,7 +20,7 @@ export default function Header() {
           </div>
           <nav className="mx-6 flex items-center space-x-4 lg:space-x-6 md:block">
             {HeaderRoutes.map((route, i) => (
-              <Button key={i} asChild variant={"ghost"}>
+              <Button key={i} asChild variant={pathName === route.href ? "default" : "ghost"}>
                 <Link
                   key={i}
                   href={route.href}
