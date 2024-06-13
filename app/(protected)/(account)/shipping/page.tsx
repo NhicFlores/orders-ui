1da6710e5f615341f0c8aa4a6239cb20df8ba934
NhicolasAponte <Nhic.Aponte@gmail.com>
2024-06-13 14:27:23 -0500

    started shipping route - stubbed in values for wrapper and form

---
+++ app/(protected)/(account)/shipping/page.tsx
@@ -0,0 +1,9 @@
+import ShippingWrapper from "./shipping-wrapper";
+
+export default function ShippingPage() {
+    return (
+        <main className="lg:w-1/2 sm:w-full border rounded-md p-4 bg-white">
+            <ShippingWrapper />
+        </main>
+    );
+}
---
+++ app/(protected)/(account)/shipping/shipping-wrapper.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+import { useState } from "react";
+
+export default function ShippingWrapper() {
+  const [isNewShippingInfo, setIsNewShippingInfo] = useState(false);
+
+  function toggleNewShippingForm() {
+    setIsNewShippingInfo(!isNewShippingInfo);
+  }
+  return (
+    <div>
+      <div className="flex justify-between w-full border-b-2 mb-4">
+        <h1 className="text-2xl font-bold">Shipping Options</h1>
+        <Button className="mb-1" onClick={toggleNewShippingForm}>
+            {isNewShippingInfo ? (
+                <div>View Shipping Options</div>
+            ) : (
+                <div className="flex">
+                <PlusIcon size={16} />
+                <div className="pl-2">New Shipping Option</div>
+                </div>
+            )}
+        </Button>
+      </div>
+      <div>
+        {isNewShippingInfo && (
+            <div>Shipping Form</div>
+        )}
+      </div>
+      <div>
+        {!isNewShippingInfo && (
+            <div>Shipping Table</div>
+        )}
+      </div>
+    </div>
+  );
+}
