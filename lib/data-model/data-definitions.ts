b5308e5d8f9fb0e4f91c023c35cc4d788278bf32
NhicolasAponte <Nhic.Aponte@gmail.com>
2024-11-07 16:54:30 -0600

    - updated schema id naming convention - created data model folder - renamed data-model.ts to schema-definitions.ts - created data-definitions.ts for containing the data types for fetch queries

---
+++ lib/data-model/data-definitions.ts
@@ -0,0 +1,19 @@
+import {
+    Order,
+    OrderInvoice,
+    OrderItem,
+    OrderStatus,
+    UserProfile,
+  } from "./schema-definitions";
+  
+  export type OrderDetails = Order &
+    Pick<OrderInvoice, "amount" | "order_invoice_id"> & {
+      ordered_by: string; //UserProfile.first_name + UserProfile.last_name
+      // order_items: OrderItem[];
+    };
+  
+  export type OrderDetailsWithItems = {
+    order_details: OrderDetails;
+    order_items: OrderItem[];
+  };
+  
