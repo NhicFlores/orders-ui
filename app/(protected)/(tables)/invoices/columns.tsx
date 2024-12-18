0db019dd20e5985b6b8a109e876f5766bf1604e4
NhicolasAponte <Nhic.Aponte@gmail.com>
2024-12-18 17:08:06 -0600

    stubbed in invoice route

---
+++ app/(protected)/(tables)/invoices/columns.tsx
@@ -0,0 +1,4 @@
+import { OrderInvoice } from "@/lib/data-model/schema-definitions";
+import { ColumnDef } from "@tanstack/react-table";
+
+export const InvoiceColumns: ColumnDef<OrderInvoice>[] = [];
---
+++ app/(protected)/(tables)/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const invoicePage = () => {
+  return (
+    <div>
+      Invoices Table
+    </div>
+  )
+}
+
+export default invoicePage
